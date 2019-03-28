e4601e73b58663f2e7b6d0555b52c33e250890bb
Yoana Todorova <dev+yoana@a2-access.com>
2019-03-28 15:28:16 +0100

    Seed with nodes and values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def create_node_values(node)
+  50.times do |i|
+    temperature = rand(-20..35)
+    humidity = rand(0..100)
+
+    node.values.create(temperature: temperature, humidity: humidity)
+  end
+end
+
+node1 = Node.create(title: 'next to reception', device_id: 1)
+create_node_values(node1)
+
+node2 = Node.create(title: 'next to tram', device_id: 2)
+create_node_values(node2)
+
+node3 = Node.create(title: 'next to kitchen', device_id: 3)
+create_node_values(node3)
