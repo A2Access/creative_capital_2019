5896408aa861379a14aa9d25ccc752cfae79ded1
Bonaventura Fleischmann and Emese Padanyi <dev+boni+emese@a2-access.com>
2019-03-28 22:15:51 +0100

    Display nodes from server response on map

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -1,18 +1,20 @@
 <template>
   <div id='map' ref="heatmap">
-    <img src="@/assets/floor_map.png" >
+    <img src="@/assets/floor_map.png">
+    <div v-show="1 == 2">
+      {{ dataPoints }}
+    </div>
   </div>
 </template>
 
 <script>
  import h337 from 'heatmap.js';
 
+ let heatmap
+
  export default {
-   created() {
-     this.$store.dispatch('fetchCurrent')
-   }
-   , mounted() {
-     const heatmap = h337.create({
+   mounted() {
+     heatmap = h337.create({
        container: this.$refs.heatmap
        /* , maxOpacity: 0.85
         * , minOpacity: 0.25*/
@@ -22,6 +24,21 @@
        max: 5,
        data: [{x:200, y:150, value: 3, radius: 200}, {x:210, y:200, value: 3, radius: 150}]
      })
+     setTimeout(() => {
+       this.$store.dispatch('fetchCurrent')
+     }, 2000)
+   }
+   , computed: {
+     dataPoints() {
+       console.log('jo')
+       if (heatmap) {
+         heatmap.setData({
+           max: 35,
+           data: this.$store.getters.newDataPoints
+         })
+       }
+       return this.$store.getters.newDataPoints
+     }
    }
  }
 </script>
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -7,20 +7,40 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   strict: true
   , state: {
-    nodeValues: {}
-
-  },
-  mutations: {
-    updateNodes() {
-
+    heatmap: null
+    , nodes: []
+  }
+  , mutations: {
+    updateNodes(state, payload) {
+      state.nodes = payload
     }
-  },
-  actions: {
-    fetchCurrent() {
-      jQuery.post('http://localhost:3000/graphql', { "query": "{ current { id title } }" })
+  }
+  , actions: {
+    fetchCurrent({ commit }, payload ) {
+      jQuery
+        .ajax({
+          type: 'POST',
+          url: 'http://localhost:3000/graphql',
+          contentType: 'application/json',
+          dataType: 'json',
+          data: { "query": "{ current { id title temperature } }" }
+        })
+        .done((data) => {
+          commit('updateNodes', data)
+        })
+        .fail(() => {
+          console.log('fetch failed')
+          const data = [
+            { id: 1, title: 'Receptionst desk', temp: 25, x: 200, y: 150, value: 15, radius: 50 }
+            , { id: 2, title: 'Raccoons corner', temp: 27, x: 240, y: 250, value: 35, radius: 40 }
+          ]
+          commit('updateNodes', data)
+        })
     }
-  },
-  getters: {
-
+  }
+  , getters: {
+    newDataPoints: (state) => {
+      return state.nodes
+    }
   }
 })
