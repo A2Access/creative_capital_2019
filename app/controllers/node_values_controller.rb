e3f1daaea28c540ab07ff282121993af98cdc1c2
Yoana Todorova <dev+yoana@a2-access.com>
2019-03-28 16:28:38 +0100

    Add endpoint for posting node data

---
+++ app/controllers/node_values_controller.rb
@@ -0,0 +1,19 @@
+class NodeValuesController < ApplicationController
+  before_action :load_node
+
+  def create
+    @node_value = @node.values.create!(node_value_params)
+
+    render json: @node_value, status: :ok
+  end
+
+  private
+
+  def load_node
+    @node = Node.find(params[:node_id])
+  end
+
+  def node_value_params
+    params.permit(:humidity, :temperature)
+  end
+end
