212fdacf34107e70838e91a02697f4492d1df5ca
Yoana Todorova <dev+yoana@a2-access.com>
2019-03-28 21:23:45 +0100

    Return all nodes with most recent stats

--- app/graphql/types/node_type.rb
+++ app/graphql/types/node_type.rb
@@ -3,5 +3,10 @@ module Types
     field :id, ID, null: false
     field :title, String, null: true
     field :device_id, Integer, null: false
+    field :temperature, Float, null: true
+
+    def temperature
+      object.values.last.temperature.to_f
+    end
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,8 +1,8 @@
 module Types
   class QueryType < Types::BaseObject
-    field :all_nodes, [NodeType], null: false
+    field :current, [NodeType], null: false
 
-    def all_nodes
+    def current
       Node.all
     end
   end
