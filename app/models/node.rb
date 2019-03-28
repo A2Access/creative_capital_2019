30bd2a0933d305b593d46d9e2ec7ee25c2afe1e8
Yoana Todorova <dev+yoana@a2-access.com>
2019-03-28 21:05:03 +0100

    Make device_id unique

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,5 +1,5 @@
 class Node < ApplicationRecord
   has_many :values, class_name: 'NodeValue'
 
-  validates :device_id, numericality: { greater_than_or_equal_to: 1 }
+  validates :device_id, numericality: { greater_than_or_equal_to: 1 }, uniqueness: true
 end
