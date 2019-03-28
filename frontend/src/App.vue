0115b0300f2be3516286cb0f8a1988e7d0e98d08
Bonaventura Fleischmann and Emese Padanyi <dev+boni+emese@a2-access.com>
2019-03-28 20:07:29 +0100

    Init frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <app-map></app-map>
+  </div>
+</template>
+
+<script>
+import Map from './components/Map'
+
+export default {
+  name: 'App',
+  components: {
+    appMap: Map
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id='map' ref="heatmap">
+    <img src="@/assets/floor_map.png" >
+  </div>
+</template>
+
+<script>
+ import $ from 'jquery';
+ import h337 from 'heatmap.js';
+
+ export default {
+   mounted() {
+     const heatmap = h337.create({
+       container: this.$refs.heatmap
+       /* , maxOpacity: 0.85
+        * , minOpacity: 0.25*/
+     })
+
+     heatmap.setData({
+       max: 5,
+       data: [{x:200, y:150, value: 3, radius: 200}, {x:210, y:200, value: 3, radius: 150}]
+     })
+   }
+ }
+</script>
+
+<style scoped>
+ #map {
+   height: 100%;
+ }
+</style>
+
